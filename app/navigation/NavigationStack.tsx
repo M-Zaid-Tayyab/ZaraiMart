bee935c06036a6824f4fad42e3fac5d05c55a708
zaid-00 <126071672+zaid-00@users.noreply.github.com>
2024-09-09 00:21:38 +0500

    All Fixes Done

--- app/navigation/NavigationStack.tsx
+++ app/navigation/NavigationStack.tsx
@@ -113,7 +113,7 @@ function MainNavigator() {
     title: route?.params?.name,
   });
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName='Main'>
       <Stack.Screen
         name="Signup"
         component={Signup}
