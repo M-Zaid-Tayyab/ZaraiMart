cdf27b23bebd1ba9ad76c0df4a96ac7cc449adc2
zaid-00 <126071672+M-Zaid-Tayyab@users.noreply.github.com>
2024-10-21 14:35:37 +0500

    Inbox integration completed

--- app/screens/Inbox/index.tsx
+++ app/screens/Inbox/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  ActivityIndicator,
   FlatList,
   SafeAreaView,
   Text,
@@ -7,16 +8,64 @@ import {
   View,
 } from 'react-native';
 import FastImage from 'react-native-fast-image';
-import { useTheme } from 'react-native-paper';
+import {useTheme} from 'react-native-paper';
 import Header from '../../components/Header';
-import { messages } from '../../utils/dummyData';
-import { useStyle } from './styles';
-import { useNavigation } from '@react-navigation/native';
+import {messages} from '../../utils/dummyData';
+import {useStyle} from './styles';
+import {useIsFocused, useNavigation} from '@react-navigation/native';
 import images from '../../config/images';
+import firestore from '@react-native-firebase/firestore';
+import {useSelector} from 'react-redux';
+import {heightPercentageToDP} from 'react-native-responsive-screen';
+
 const Inbox: React.FC = () => {
   const styles = useStyle();
-  const navigation=useNavigation();
+  const navigation = useNavigation();
   const theme = useTheme();
+  const [isLoading, setIsLoading] = useState(true);
+  const isFocused = useIsFocused();
+  const user = useSelector(state => state?.userReducer?.user);
+  const [inbox, setInbox] = useState();
+  function formatFirebaseTimestamp(firebaseTimestamp) {
+    const dateObject = firebaseTimestamp.toDate();
+    const now = new Date();
+    const isSameDay = (date1, date2) =>
+      date1.getDate() === date2.getDate() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getFullYear() === date2.getFullYear();
+
+    const isYesterday = date => {
+      const yesterday = new Date(now);
+      yesterday.setDate(now.getDate() - 1);
+      return isSameDay(date, yesterday);
+    };
+
+    const isSameWeek = date => {
+      const weekStart = new Date(now);
+      weekStart.setDate(now.getDate() - now.getDay());
+      return date >= weekStart;
+    };
+    const formatDate = date => {
+      const day = date.getDate().toString().padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+    };
+    if (isSameDay(dateObject, now)) {
+      return dateObject.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+    }
+    if (isYesterday(dateObject)) {
+      return 'Yesterday';
+    }
+    if (isSameWeek(dateObject)) {
+      return dateObject.toLocaleDateString('en-US', {weekday: 'long'});
+    }
+    return formatDate(dateObject);
+  }
+
   const Message = props => (
     <TouchableOpacity style={styles.rowContainer} onPress={props?.onPress}>
       <FastImage
@@ -42,23 +91,74 @@ const Inbox: React.FC = () => {
       </View>
     </TouchableOpacity>
   );
-  const renderChat = ({item}) => (
-    <Message
-      leftIcon={item?.leftIcon}
-      user={item?.user}
-      message={item?.message}
-      unread={item?.unread}
-      date={item?.date}
-      onPress={()=>{navigation.navigate("Chat");}}
-    />
-  );
+  const renderChat = ({item}) => {
+    return (
+      <Message
+        leftIcon={
+          item?.otherUserData?.profileUrl || images.Home.userPlaceholder
+        }
+        user={item?.otherUserData?.name}
+        message={item?.chatRoomData?.lastMessage}
+        unread={item?.unread}
+        date={formatFirebaseTimestamp(item?.chatRoomData?.lastMessageDate)}
+        onPress={() => {
+          navigation.navigate('Chat', {otherUserId: item?.otherUserId});
+        }}
+      />
+    );
+  };
+  const getUserChatsWithOtherUser = async () => {
+    try {
+      const userDataList = [];
+      const querySnapshot = await firestore()
+        .collection('chats')
+        .where('participants', 'array-contains', user?.uid)
+        .get();
+      for (const doc of querySnapshot.docs) {
+        const chatRoomData = doc.data();
+        const participants = chatRoomData.participants;
+        const otherUserId = participants.find(id => id !== user?.uid);
+
+        if (otherUserId) {
+          const otherUserSnapshot = await firestore()
+            .collection('users')
+            .doc(otherUserId)
+            .get();
+
+          if (otherUserSnapshot.exists) {
+            userDataList.push({
+              chatRoomId: doc.id,
+              otherUserData: otherUserSnapshot.data(),
+              otherUserId: otherUserId,
+              chatRoomData: chatRoomData,
+            });
+          }
+        }
+      }
+
+      setInbox(userDataList);
+    } catch (err) {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    getUserChatsWithOtherUser();
+  }, [isFocused]);
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.subContainer}>
-        <Header title='Inbox' />
-        <View style={styles.FlatListContainer}>
-          <FlatList renderItem={renderChat} data={messages} />
-        </View>
+        <Header title="Inbox" />
+        {isLoading ? (
+          <ActivityIndicator
+            style={{marginTop: heightPercentageToDP(2)}}
+            color={theme.colors.primaryButton}
+            size={'large'}></ActivityIndicator>
+        ) : (
+          <View style={styles.FlatListContainer}>
+            <FlatList renderItem={renderChat} data={inbox} />
+          </View>
+        )}
       </View>
     </SafeAreaView>
   );
